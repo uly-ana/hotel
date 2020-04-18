0f6ca67be188ddbd30585e93b63538f7a3c556c6
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-18 16:46:56 +0300

    added Navbar ccomponent to all pages

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
-
 import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/rooms/" component={Rooms} />
