6f5d5e878d9c690ead5e7a16d023a6fd4c944c80
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-18 14:55:13 +0300

    imported all pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 
 function App() {
   return (
