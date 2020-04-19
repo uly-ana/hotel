0263b12e8843e15a7102a9845d6056ed14749998
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 18:38:29 +0300

    added Banner ro Rooms page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from "../components/Hero";
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
 
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero"/>
+        <Hero hero="roomsHero">
+            <Banner title="Our Rooms">
+                <Link to="./" className="btn-primary">Return Home</Link>
+            </Banner>
+        </Hero>
     )
 }
 
