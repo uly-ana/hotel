b6956547add32373fc56d8691450502c4150d70d
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 18:14:44 +0300

    imported Hero to Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import Hero from "../components/Hero";
+
 
 export default function Error() {
     return (
-        <div>
-            Hello from error page
-        </div>
+        <Hero />
     )
 }
