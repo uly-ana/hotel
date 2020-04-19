3c90844303f0dd531e48520fff06389856e7503a
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 19:21:02 +0300

    compeated Services section: added icons, title, and info

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa'
 import Title from './Title'
 
 
 export default class Services extends Component {
-    render() {
-        return (
-            <div>
-                Hello from services
-            </div>
-        )
-    }
-}
+                 state = {
+                   services: [
+                     {
+                       icon: <FaCocktail />,
+                       title: "free cocktails",
+                       info: "some info about cocktails"
+                     },
+                     {
+                       icon: <FaHiking />,
+                       title: "hiking",
+                       info: "some info about hiking"
+                     },
+                     {
+                       icon: <FaShuttleVan/>,
+                       title: "free shuttle bus",
+                       info: "some info about transportation"
+                     },
+                     {
+                       icon: <FaBeer />,
+                       title: "free beer",
+                       info: "some info about free beer",
+                     }
+                   ],
+                 };
+                 render() {
+                   return (
+                     <section className="services">
+                       <Title title="services"/>
+                       <div className="services-center">
+                           {this.state.services.map((item, index) => { 
+                               return <acticle key={index} className="service">
+<span>{item.icon}</span>
+                           <h6>{item.title}</h6>
+                           <p>{item.info}</p>
+                               </acticle>
+
+                           })}
+                       </div>
+                     </section>
+                   );
+                 }
+               }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 
 export default function Home() {
     return (
+        <>
         <Hero hero='defaultHero'>
             <Banner title="luxurious rooms" subtitle="rooms starting 299$">
                 <Link to="/rooms" className="btn-primary">Our Rooms</Link>
                 </Banner>
         </Hero>
+        <Services/>
+        </>
     )
 }
