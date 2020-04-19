ba07c5e9bb594a84545569e93a558379056541c8
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 15:38:28 +0300

    added and styled logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import logo from '../images/logo.svg'
+import {FaAlignRight} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 export default class Navbar extends Component {
+    state={
+        isOpen: false
+    }
+    handleToggle = ()=> {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
     render() {
         return (
-            <div>
-                <h1>helo from navbar</h1>
-            </div>
+            <nav className="navbar">
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/"> 
+                             <img src={logo} alt="Hotel Resort"/>
+                        </Link>
+                    </div>
+                </div>    
+            </nav>
         )
     }
 }
