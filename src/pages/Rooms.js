09711d873f7df92e1b5eb7485f4fddfb465cefe0
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 18:13:14 +0300

    added different hero for rooms page

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import Hero from "../components/Hero";
+
 
 const Rooms = () => {
     return (
-        <div>
-            <h1>Hello from rooms</h1>
-        </div>
+        <Hero hero="roomsHero"/>
     )
 }
 
