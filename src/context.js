f7c46a69ae403c9db976259f78e28acab36a9539
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 23:04:23 +0300

    syntax fix

--- src/context.js
+++ src/context.js
@@ -12,14 +12,14 @@ export default class RoomProvider extends Component {
     };
 
     componentDidMount(){
-        let rooms = this.formatData(items)
+        let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
         this.setState({
             rooms, 
             featuredRooms, 
             sortedRooms: rooms, 
             loading: false
-        })
+        });
     }
 
     formatData(items) {
