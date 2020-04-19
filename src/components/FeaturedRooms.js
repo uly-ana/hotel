a046e79268bf8be99efc198ab76d18bb709fde83
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 22:28:50 +0300

    init data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,7 +4,7 @@ import {RoomContext} from '../context';
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-
+        const {featuredRooms: rooms} = this.context
         return (
             <div>
                 hello from Frooms 
