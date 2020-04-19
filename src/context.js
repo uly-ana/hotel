33ac778e4207cf1370a9279c1768e3456b2b7c3f
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 21:48:05 +0300

    created context

---
+++ src/context.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import items from './data'
+
+const RoomContext = React.createContext();
+
+export default class RoomProvider extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <RoomContext.Provider value={{...this.state}}>
+                {this.props.children}
+            </RoomContext.Provider>
+        )
+    }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export {RoomProvider,RoomConsumer, RoomContext};
