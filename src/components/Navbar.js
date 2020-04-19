5dd7552a5639697253b14fcb1cc1f2abd67d8c78
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-19 15:49:07 +0300

    compleated Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,19 +13,32 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar">
-                <div className="nav-center">
-                    <div className="nav-header">
-                        <Link to="/"> 
-                             <img src={logo} alt="Hotel Resort"/>
-                        </Link>
-                        <button type="button" className="nav-btn" onClick={this.handleToggle}>
-                            <FaAlignRight className="nav-icon"/>
-                        </button>
-                    </div>
-                </div>
-                    
-            </nav>
-        )
+          <nav className="navbar">
+            <div className="nav-center">
+              <div className="nav-header">
+                <Link to="/">
+                  <img src={logo} alt="Hotel Resort" />
+                </Link>
+                <button
+                  type="button"
+                  className="nav-btn"
+                  onClick={this.handleToggle}
+                >
+                  <FaAlignRight className="nav-icon" />
+                </button>
+              </div>
+            </div>
+            <ul
+              className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/rooms">Rooms</Link>
+              </li>
+            </ul>
+          </nav>
+        );
     }
 }
