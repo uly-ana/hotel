d91df454a618ff0db7a7e08a2b036588947e6b2f
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-20 17:20:34 +0300

    added hero to single room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -6,11 +6,36 @@ import {Link} from 'react-router-dom'
 import {RoomContext} from '../context'
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            slug: this.props.match.params.slug,
+            defaultBcg: defaultBcg
+        }
+    }
+    static contextType = RoomContext;
+    // componentDidMount(){}
+
     render() {
-        return (
-            <div>
-                hello from single room page
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room){
+            return (<div className="error">
+                <h3>Not such room could be found ... </h3>
+                <Link to='/rooms' className="btn-primary">Back to rooms</Link>
             </div>
+            );
+        }
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+
+        return (
+            <Hero hero="roomsHero">
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>
+                        back to rooms
+                    </Link>
+                </Banner>
+            </Hero>
         )
     }
 }
