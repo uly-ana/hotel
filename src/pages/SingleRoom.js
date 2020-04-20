06b0e9b0e87b85fbb5c28261b2a6da8eaa55d8d0
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-20 16:48:00 +0300

    imported all components to a single room page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../context'
 
 export default class SingleRoom extends Component {
     render() {
