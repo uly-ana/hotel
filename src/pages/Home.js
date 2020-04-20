ed90a4999752047d457929620fd792aecbb2ccb7
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-20 19:25:51 +0300

    installed styled components, created Styled Hero  and imported to single room

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import Services from '../components/Services'
 import FeaturedRooms from '../components/FeaturedRooms'
 
+
 export default function Home() {
     return (
         <>
@@ -15,6 +16,7 @@ export default function Home() {
         </Hero>
         <Services/>
         <FeaturedRooms/>
+        
         </>
     )
 }
