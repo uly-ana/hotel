a8bf37a5c1bf544e33fc272256a9d387b6def1f5
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-21 23:40:20 +0300

    compleated room List

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Room from './Room'
 
-export default function RoomList() {
+export default function RoomList({rooms}) {
+if (rooms.length === 0) {
     return (
-        <div>
-            Hello from room list
-        </div>
+       <div className="empty-search">
+           <h3>Unfortunately no rooms matched your search parametrs</h3>
+       </div> 
+    )
+} return (
+    <section className="roomslist">
+     <div className="roomslist-center">
+         {
+             rooms.map(item => {
+                 return <Room key={item.id} room={item}/>
+             })
+         }
+    </div>   
+    </section>  
     )
 }
