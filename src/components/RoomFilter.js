91493822e4717f103d095e86c48e20b8f6798d8d
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-22 12:48:23 +0300

    added options to the filter room-type

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,12 +3,30 @@ import {useContext} from 'react'
 import {RoomContext} from '../context'
 import Title from '../components/Title'
 
-export default function RoomFilter() {
+const getUnique = (item, value) =>{
+    return [...new Set(item.map(item => item[value]))]
+}
+
+export default function RoomFilter({rooms}) {
     const context = useContext(RoomContext);
     const {
-        handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets
+        handleChange, 
+        type, 
+        capacity, 
+        price, 
+        minPrice, 
+        maxPrice, 
+        minSize, 
+        maxSize, 
+        breakfast, 
+        pets
     } = context;
     
+    let types = getUnique(rooms, 'type');
+    types = ['all',...types];
+    types = types.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+    })
 
     return (
         <section className="filter-container">
@@ -17,8 +35,10 @@ export default function RoomFilter() {
                 {/* select type */}
                 <div className="form-group">
                 <label htmlFor="type">room type</label>
-                <select name="type" id="type" value={type} className="form-control" onChange={handleChange}></select>
-
+                <select name="type" id="type" value={type} className="form-control" onChange={handleChange}
+                >
+                {types}
+                </select>
                 </div>
                 {/* end select type */}
             </form>
