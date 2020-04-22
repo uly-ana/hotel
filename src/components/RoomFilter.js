e36e41a158d30aa4297c07c815ea71d9d835b51b
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-22 13:37:56 +0300

    added capacity filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -23,26 +23,56 @@ export default function RoomFilter({rooms}) {
     } = context;
     
     let types = getUnique(rooms, 'type');
+
     types = ['all',...types];
+
     types = types.map((item, index) => {
-    return <option value={item} key={index}>{item}</option>
+        return (
+            <option value={item} key={index}>{item}</option>
+        )
+    });
+
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+        return (
+        <optin key={index} value={item}>{item}</optin>
+        )
     })
 
     return (
-        <section className="filter-container">
-            <Title title="search rooms"/>
-            <form className="filter-form">
-                {/* select type */}
-                <div className="form-group">
-                <label htmlFor="type">room type</label>
-                <select name="type" id="type" value={type} className="form-control" onChange={handleChange}
-                >
-                {types}
-                </select>
-                </div>
-                {/* end select type */}
-            </form>
-        </section>
-    )
+      <section className="filter-container">
+        <Title title="search rooms" />
+        <form className="filter-form">
+          {/* select type */}
+          <div className="form-group">
+            <label htmlFor="type">room type</label>
+            <select
+              name="type"
+              id="type"
+              value={type}
+              className="form-control"
+              onChange={handleChange}
+            >
+              {types}
+            </select>
+          </div>
+          {/* end select type */}
+          {/* select capacity */}
+          <div className="form-group">
+            <label htmlFor="capacity">Guest</label>
+            <select
+              name="capacity"
+              id="capacity"
+              value={capacity}
+              className="form-control"
+              onChange={handleChange}
+            >
+              {people}
+            </select>
+          </div>
+          {/* end of capacity */}
+        </form>
+      </section>
+    );
 }
 
