86b772e8deb22ca1e365b394d48202b59ea5c7b7
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-22 13:09:17 +0300

    cpmpleated filter typr-room

--- src/context.js
+++ src/context.js
@@ -56,13 +56,27 @@ class RoomProvider extends Component {
     };
 
     handleChange = event => {
-        const type = event.target.type
-        const name = event.target.type;
-        const value  = event.target.type;
+        const target = event.target
+        const value = event.type === 'checkbox' ? target.checked : target.value
+        const name  = event.target.name
+        this.setState({
+            [name]: value
+        }, this.filterRooms)
     };
 
     filterRooms = () => {
-        console.log("hello")
+       let {
+           rooms, type, capacity, price,minSize, maxSize,
+           breakfast, pets
+       } = this.state;
+
+       let tempRooms = [...rooms];
+       if(type !== 'all') {
+           tempRooms = tempRooms.filter(room => room.type === type)
+       }
+       this.setState({
+           sortedRooms: tempRooms
+       })
     }
 
     render() {
