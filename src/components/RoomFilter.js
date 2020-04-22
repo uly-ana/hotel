a513b0cbbdf6b520e3e2b68a61cfbde7271d9a47
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-22 14:44:02 +0300

    added pricr filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -40,37 +40,45 @@ export default function RoomFilter({rooms}) {
     });
 
     return (
-      <section className="filter-container">
+    <section className="filter-container">
         <Title title="search rooms" />
         <form className="filter-form">
           {/* select type */}
-          <div className="form-group">
+        <div className="form-group">
             <label htmlFor="type">room type</label>
-            <select
-              name="type"
-              id="type"
-              value={type}
-              className="form-control"
-              onChange={handleChange}
-            >
-              {types}
-            </select>
-          </div>
-          {/* end select type */}
-          {/* select capacity */}
-          <div className="form-group">
-            <label htmlFor="capacity">Guests</label>
-            <select
-              name="capacity"
-              id="capacity"
-              value={capacity}
-              className="form-control"
-              onChange={handleChange}
-            >
-              {people}
-            </select>
-          </div>
+    <select
+        name="type"
+        id="type"
+        value={type}
+        className="form-control"
+        onChange={handleChange}>
+            {types}
+    </select>
+        </div>
+    {/* end select type */}
+    {/* select capacity */}
+    <div className="form-group">
+        <label htmlFor="capacity">Guests</label>
+    <select
+        name="capacity"
+        id="capacity"
+        value={capacity}
+        className="form-control"
+        onChange={handleChange}>
+        {people}
+    </select>
+    </div>
           {/* end of capacity */}
+          {/* room price */}
+    <div className="form-group">
+    <label htmlFor="price">room price ${price}</label>
+    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control"></input>
+
+    </div>
+
+
+
+          {/* end room price */}
         </form>
       </section>
     );
--- src/context.js
+++ src/context.js
@@ -65,30 +65,31 @@ class RoomProvider extends Component {
     };
 
     filterRooms = () => {
-       let {
-           rooms, type, capacity, price,minSize, maxSize,
-           breakfast, pets
-       } = this.state;
+    let {
+        rooms, type, capacity, price,minSize, maxSize,
+        breakfast, pets
+    } = this.state;
 
-       let tempRooms = [...rooms];
-       capacity = parseInt(capacity)
+    let tempRooms = [...rooms];
+    capacity = parseInt(capacity)
 
     //    filter by type
-       if(type !== 'all') {
-           tempRooms = tempRooms.filter(room => room.type === type)
-       }
+    if(type !== 'all') {
+        tempRooms = tempRooms.filter(room => room.type === type)
+    }
 
     // filter by capacity
-       if (capacity !== 1){
-            tempRooms = tempRooms.filter(room => 
-                room.capacity >= capacity)
-            }
-
-
-       this.setState({
-           sortedRooms: tempRooms
-       })
+    if (capacity !== 1){
+        tempRooms = tempRooms.filter(room => 
+            room.capacity >= capacity)
     }
+
+
+    this.setState({
+        sortedRooms: tempRooms
+    })
+    
+    };
 
     render() {
         return (
